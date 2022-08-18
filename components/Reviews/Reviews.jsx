6f45e6c444ac1afe8191230981aec5a833314622
Maxim Lebiga <lebiga.maks@gmail.com>
2022-08-18 14:55:51 +0300

    add snow

--- components/Reviews/Reviews.jsx
+++ components/Reviews/Reviews.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react"
 import Img from "../../images/reviews/image.png"
 import style from "./Reviews.module.css"
 import star from "../../images/reviews/star.png"
+import Snow from "../../images/reviews/snow.svg"
 
 const Reviews = ({rating}) => {
   const [starsArray, setStarsArray] = useState([])
@@ -15,37 +16,40 @@ const Reviews = ({rating}) => {
   }, [rating])
 
   return (
-    <div className={style.wrapper}>
-      <div>
-        <h2 className={style.title}>What Path User Say</h2>
-        <div className={style.content_wrapper}>
-          <div className={style.image}>
-            <Image src={Img} alt="" />
-          </div>
-          <div className={style.content}>
-            <p className={style.text}>
-              <span>“</span>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-              vulputate libero et velit
-            </p>
-            <div className={style.author_wrapper}>
-              <div className={style.line_wrapper}>
-                <div className={style.line} />
-                <div className={style.cicle} />
-              </div>
-              <p className={style.author}>Edith Lowe</p>
+    <div className={style.section}>
+      <div className={style.wrapper}>
+        <div>
+          <h2 className={style.title}>What Path User Say</h2>
+          <div className={style.content_wrapper}>
+            <div className={style.image}>
+              <Image src={Img} alt="" />
             </div>
-            <div className={style.rating}>
-              {starsArray.length > 0 &&
-                starsArray.map((star, index) => (
-                  <div className={style.star} key={index}>
-                    <Image src={star} alt="" />
-                  </div>
-                ))}
+            <div className={style.content}>
+              <p className={style.text}>
+                <span>“</span>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                vulputate libero et velit
+              </p>
+              <div className={style.author_wrapper}>
+                <div className={style.line_wrapper}>
+                  <div className={style.line} />
+                  <div className={style.cicle} />
+                </div>
+                <p className={style.author}>Edith Lowe</p>
+              </div>
+              <div className={style.rating}>
+                {starsArray.length > 0 &&
+                  starsArray.map((star, index) => (
+                    <div className={style.star} key={index}>
+                      <Image src={star} alt="" />
+                    </div>
+                  ))}
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <Snow className={style.snow} />
     </div>
   )
 }
